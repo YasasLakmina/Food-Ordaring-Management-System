acf9b4638996f1d0f728dd732dbd7b97a4fdb73b
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2025-04-17 12:31:06 +0530

    order model

---
+++ Backend/models/orderModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    restaurantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Restaurant", // Reference to the Restaurant model
+      required: true,
+    },
+    customerName: {
+      type: String,
+      required: true,
+    },
+    customerContact: {
+      type: String,
+      required: true,
+    },
+    items: [
+      {
+        menuItemId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "MenuItem", // Reference to the MenuItem model
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "preparing", "ready", "completed", "cancelled"],
+      default: "pending",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
